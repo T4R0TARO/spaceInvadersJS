bd593a2c6bf64fe6ed9b026fa7a68acd3c7c7896
josh manansala <joshmanansalagsd@gmail.com>
2021-11-05 19:55:53 -0700

    update logic

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Create and select default variables for later use
 const grid = document.querySelector('.grid')
 const resultsDisplay = document.querySelector('.results')
 let currentShooterIndex = 202
@@ -8,19 +9,24 @@ let goingRight = true
 let aliensRemoved = []
 let results = 0
 
+// for every loop; create an element div and have the new become the a child of the grid variable 
 for (let i = 0; i < 225; i++) {
   const square = document.createElement('div')
   grid.appendChild(square)
 }
 
+// Creates a shallow copy of the array 
 const squares = Array.from(document.querySelectorAll('.grid div'))
 
+
+// set default valules positions to the alien invaders on the grid 
 const alienInvaders = [
   0,1,2,3,4,5,6,7,8,9,
   15,16,17,18,19,20,21,22,23,24,
   30,31,32,33,34,35,36,37,38,39
 ]
 
+// create the invaders on the grid 
 function draw() {
   for (let i = 0; i < alienInvaders.length; i++) {
     if(!aliensRemoved.includes(i)) {
@@ -28,18 +34,22 @@ function draw() {
     }
   }
 }
-
+// call function
 draw()
 
+
+// removes ivader from grid by removing class name 
 function remove() {
   for (let i = 0; i < alienInvaders.length; i++) {
     squares[alienInvaders[i]].classList.remove('invader')
   }
 }
 
+//adds the shooter position on the grid by add the class name 
 squares[currentShooterIndex].classList.add('shooter')
 
 
+// move the index of the shoot by key press event listener 
 function moveShooter(e) {
   squares[currentShooterIndex].classList.remove('shooter')
   switch(e.key) {
@@ -54,6 +64,7 @@ function moveShooter(e) {
 }
 document.addEventListener('keydown', moveShooter)
 
+// moves invader left and right so that the invader are not idle
 function moveInvaders() {
   const leftEdge = alienInvaders[0] % width === 0
   const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width -1
